7d6ba0b92b20ec7de24b4f566d1506956d54cace
qaate47 <antoine.willerval@the-qa-company.com>
2025-04-01 22:13:43 +0200

    radiant keys decompiler

---
+++ src/acts/tools/bo6/bo6.cpp
@@ -0,0 +1,68 @@
+#include <includes.hpp>
+#include <core/bytebuffer.hpp>
+namespace {
+	enum RadianKeysType : byte {
+		RKT_UNK0 = 0,
+		RKT_UNK1 = 1,
+		RKT_STRING = 2,
+		RKT_UNK3 = 3,
+		RKT_VECTOR = 4,
+		RKT_UNK5 = 5,
+		RKT_FLOAT = 6,
+		RKT_INTEGER = 7,
+		RKT_UNK8 = 8,
+		RKT_HASH_LOCALIZED = 9,
+	};
+
+	const char* RadianKeysTypeName(RadianKeysType type) {
+		switch (type) {
+		case RKT_STRING: return "string";
+		case RKT_VECTOR: return "vector";
+		case RKT_FLOAT: return "float";
+		case RKT_INTEGER: return "int";
+		case RKT_HASH_LOCALIZED: return "localized";
+		default: return utils::va("unk%d", type);
+		}
+	}
+
+	int bo6_radiant_keys(int argc, const char* argv[]) {
+		if (tool::NotEnoughParam(argc, 1)) return tool::BAD_USAGE;
+		std::vector<byte> data{};
+		if (!utils::ReadFile(argv[2], data)) {
+			LOG_ERROR("Can't read {}", argv[2]);
+			return tool::BASIC_ERROR;
+		}
+		std::filesystem::path out;
+
+		if (tool::NotEnoughParam(argc, 2)) {
+			out = argv[2];
+			out.replace_extension(".dec.txt");
+		}
+		else {
+			out = argv[3];
+		}
+
+		utils::OutFileCE os{ out };
+		if (!os) {
+			LOG_ERROR("Can't open {}", argv[3]);
+			return tool::BASIC_ERROR;
+		}
+		core::bytebuffer::ByteBuffer buffer{ data };
+
+		while (buffer.CanRead(sizeof(uint64_t))) {
+			uint64_t key{ buffer.Read<uint64_t>() };
+
+			if (!key) break;
+
+			RadianKeysType type{ buffer.Read<RadianKeysType>() };
+
+			os << std::setw(10) << std::setfill(' ') << std::left << RadianKeysTypeName(type) << " " << hashutils::ExtractTmp("hash", key) << "\n";
+		}
+
+		LOG_INFO("Dump into {}", out.string());
+
+		return tool::OK;
+	}
+
+	ADD_TOOL(bo6_radiant_keys, "bo6", " [keys] [out]", "Read bo6 radiant keys", bo6_radiant_keys);
+}
