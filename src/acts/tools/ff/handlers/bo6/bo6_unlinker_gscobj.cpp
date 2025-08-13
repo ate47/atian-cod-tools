a17981d5b2da8597f07e917f029d44ac526c13a5
qaate47 <antoine.willerval@the-qa-company.com>
2025-08-13 08:34:43 +0200

    decompile bo6 gscobj when unlinking ff

--- src/acts/tools/ff/handlers/bo6/bo6_unlinker_gscobj.cpp
+++ src/acts/tools/ff/handlers/bo6/bo6_unlinker_gscobj.cpp
@@ -1,5 +1,6 @@
 #include <includes.hpp>
 #include <tools/ff/handlers/handler_game_bo6.hpp>
+#include <tools/gsc_decompiler.hpp>
 
 namespace {
 	using namespace fastfile::handlers::bo6;
@@ -10,7 +11,7 @@ namespace {
 			struct GscObj {
 				uint64_t name;
 				uint32_t len;
-				void* buffer;
+				byte* buffer;
 			}; static_assert(sizeof(GscObj) == 0x18);
 			GscObj* asset{ (GscObj*)ptr };
 
@@ -21,6 +22,37 @@ namespace {
 
 			if (!utils::WriteFile(outFile, asset->buffer, asset->len)) {
 				LOG_ERROR("Error when dumping {}", outFile.string());
+			}
+
+			if (!opt.disableScriptsDecomp) {
+				tool::gsc::GscDecompilerGlobalContext gdctx{};
+
+				std::filesystem::path outSourceGDB{ opt.m_output / "bo6" / "source" / "gdb" };
+				std::string outSourceGDBStr{ outSourceGDB.string() };
+				gdctx.opt.m_generateGdbData = true;
+				gdctx.opt.m_generateGdbBaseData = false;
+				gdctx.opt.m_dbgOutputDir = outSourceGDBStr.data();
+				gdctx.opt.m_platform = tool::gsc::opcode::PLATFORM_PC; // todo: use header type
+				gdctx.opt.m_dcomp = true;
+				std::filesystem::path outSource{ opt.m_output / "bo6" / "source" };
+				std::string outSourceStr{ outSource.string() };
+				gdctx.opt.m_outputDir = outSourceStr.data();
+
+				std::vector<byte> scriptData;
+				byte* alignedAlloc{ utils::Aligned(asset->buffer, 0x20) };
+
+				if (asset->buffer != alignedAlloc) {
+					// we clone the buffer to align it
+					utils::Allocate(scriptData, asset->len + 0x20);
+					alignedAlloc = utils::Aligned(scriptData.data(), 0x20);
+					std::memcpy(alignedAlloc, asset->buffer, asset->len);
+				}
+
+				int r{ tool::gsc::DecompileGsc(alignedAlloc, asset->len, outFile, gdctx) };
+
+				if (r) {
+					LOG_ERROR("Error when decompiling script");
+				}
 			}
 		}
 	};
