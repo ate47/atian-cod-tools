122539fe57a2fcf0919fdcbe18d9c42dabb88c38
qaate47 <antoine.willerval@the-qa-company.com>
2025-11-08 23:09:03 +0100

    decompress cw spt using ff unlinker

--- src/acts/tools/ff/handlers/cw/cw_unlinker_scriptparsetree.cpp
+++ src/acts/tools/ff/handlers/cw/cw_unlinker_scriptparsetree.cpp
@@ -2,6 +2,7 @@
 #include <core/raw_file_json.hpp>
 #include <tools/utils/raw_file_extractor.hpp>
 #include <tools/ff/handlers/handler_game_cw.hpp>
+#include <tools/gsc_decompiler.hpp>
 
 namespace {
 	using namespace fastfile::handlers::cw;
@@ -12,7 +13,7 @@ namespace {
 		void Unlink(fastfile::FastFileOption& opt, void* ptr) {
 			struct ScriptParseTree {
 				CWXHash name;
-				void* buffer;
+				byte* buffer;
 				uint32_t len;
 			}; static_assert(sizeof(ScriptParseTree) == 0x18);
 			ScriptParseTree* asset{ (ScriptParseTree*)ptr };
@@ -24,6 +25,38 @@ namespace {
 
 			if (!utils::WriteFile(outFile, asset->buffer, asset->len)) {
 				LOG_ERROR("Error when dumping {}", outFile.string());
+			}
+
+			if (!opt.disableScriptsDecomp) {
+
+				tool::gsc::GscDecompilerGlobalContext gdctx{};
+
+				std::filesystem::path outSourceGDB{ opt.m_output / "cw" / "source" / "gdb" };
+				std::string outSourceGDBStr{ outSourceGDB.string() };
+				gdctx.opt.m_generateGdbData = true;
+				gdctx.opt.m_generateGdbBaseData = false;
+				gdctx.opt.m_dbgOutputDir = outSourceGDBStr.data();
+				gdctx.opt.m_platform = fastfile::GetCurrentContext().gscPlatform;
+				gdctx.opt.m_dcomp = true;
+				std::filesystem::path outSource{ opt.m_output / "cw" / "source" };
+				std::string outSourceStr{ outSource.string() };
+				gdctx.opt.m_outputDir = outSourceStr.data();
+
+				std::vector<byte> scriptData;
+				byte* alignedAlloc{ utils::Aligned(asset->buffer, 0x20) };
+
+				if (asset->buffer != alignedAlloc) {
+					// we clone the buffer to align it
+					utils::Allocate(scriptData, asset->len + 0x20);
+					alignedAlloc = utils::Aligned(scriptData.data(), 0x20);
+					std::memcpy(alignedAlloc, asset->buffer, asset->len);
+				}
+
+				int r{ tool::gsc::DecompileGsc(alignedAlloc, asset->len, outFile, gdctx) };
+
+				if (r) {
+					LOG_ERROR("Error when decompiling script");
+				}
 			}
 		}
 	};
