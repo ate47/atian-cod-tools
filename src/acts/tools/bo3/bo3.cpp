dd4d39f51488194831127313bc4ad8ecf85860cf
qaate47 <antoine.willerval@the-qa-company.com>
2025-12-12 20:12:48 +0100

    bo3 clump container dumper

--- src/acts/tools/bo3/bo3.cpp
+++ src/acts/tools/bo3/bo3.cpp
@@ -1,7 +1,8 @@
 #include <includes.hpp>
-#include "pools.hpp"
-#include "bo3.hpp"
+#include <tools/bo3/bo3.hpp>
+#include <tools/bo3/pools.hpp>
 #include <tools/gsc_vm/vm_t7.hpp>
+#include <core/bytebuffer.hpp>
 
 namespace bo3 {
     int InjectScriptBO3(Process& proc, const char* script, const char* replaced, std::string& notify) {
@@ -138,5 +139,102 @@ namespace {
 
         return ret;
     }
+
+	int clumpdump(int argc, const char* argv[]) {
+		if (tool::NotEnoughParam(argc, 2)) return tool::BAD_USAGE;
+
+		std::vector<byte> clump{ utils::ReadFile<std::vector<byte>>(argv[2]) };
+		std::filesystem::path outputDir{ argv[3] };
+
+		struct ClumpFile {
+			uint32_t name;
+			uint32_t fileSectorOffset;
+			uint64_t fileSize;
+		};
+
+		core::bytebuffer::ByteBuffer reader{ clump };
+		constexpr size_t chunkSize = 0x400;
+		constexpr size_t blockSize = 0x1000;
+
+		size_t count{};
+		ClumpFile* files{ reader.Ptr<ClumpFile>() };
+		while (reader.CanRead(chunkSize * sizeof(ClumpFile))) {
+			ClumpFile* tmp{ reader.ReadPtr<ClumpFile>(chunkSize) };
+			if (tmp[chunkSize - 1].name) {
+				count += chunkSize;
+				continue; // last file not empty, we continue
+			}
+
+			// last chunk, we read the count
+			while (tmp->name) {
+				tmp++;
+				count++;
+			}
+
+			break;
+		}
+
+		LOG_INFO("Found {} files", count);
+
+		// find names
+		size_t end{};
+		for (size_t i = 0; i < count; i++) {
+			size_t idx{ ((size_t)files[i].fileSectorOffset * blockSize) + files[i].fileSize };
+			if (end < idx) {
+				end = idx;
+			}
+		}
+
+		end = utils::Aligned(end, 0x1000);
+		LOG_DEBUG("end idx : 0x{:x}", end);
+		reader.Goto(end);
+
+		// compute toc
+		std::unordered_map<uint32_t, const char*> filenames{};
+
+		for (size_t i = 0; i < count; i++) {
+			size_t len, hlen;
+			char* fh{ reader.ReadString(&hlen, ':')};
+			char* fn{ reader.ReadString(&len, 0x0A) };
+			fh[hlen] = 0;
+			fn[len] = 0;
+			LOG_DEBUG("Dump file {} ({})", fn, fh);
+
+			uint32_t hash{ std::strtoul(fh, nullptr, 16) };
+
+			filenames[hash] = fn;
+		}
+
+		for (size_t i = 0; i < count; i++) {
+			ClumpFile& file{ files[i] };
+
+			size_t offset{ file.fileSectorOffset * blockSize };
+
+			auto it{ filenames.find(file.name) };
+			const char* name;
+			if (it != filenames.end()) {
+				name = it->second;
+			}
+			else {
+				LOG_ERROR("Unindexed file name: 0x{:x}", file.name);
+				name = utils::va("invalid_%x.bin", file.name);
+			}
+
+			std::filesystem::path outputFile{ outputDir / name };
+			std::filesystem::create_directories(outputFile.parent_path());
+			reader.Goto(offset);
+			byte* fileData{ reader.ReadPtr<byte>(file.fileSize) };
+			if (!utils::WriteFile(outputFile, fileData, file.fileSize)) {
+				LOG_ERROR("Can't dump {}", outputFile.string());
+			}
+			else {
+				LOG_INFO("Dump {}", outputFile.string());
+			}
+		}
+
+
+		return tool::OK;
+	}
 }
 ADD_TOOL(injectbo3, "bo3", " (script) (replace)", "inject script (bo3)", L"BlackOps3.exe", injectbo3);
+ADD_TOOL(clumpdump, "bo3", " (file) (output)", "dump clump container", clumpdump);
